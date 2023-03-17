7bd831b63b58fbf612c67a9c3418bc79480ce1c6
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-03-16 22:36:04 -0300

    Aula02

---
+++ Aula02/src/aula02/Aula02.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula02;
+
+/**
+ *
+ * @author joao.vcsouza16
+ */
+import java.util.Scanner;
+public class Aula02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Ler um número inteiro n. escrever a soma de todos os números de 1 até n.
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número para ser achado");
+        int num = entrada.nextInt();
+        int soma = 0;
+        int som1 = 0;
+        
+        
+//            }
+         int vetor[] = new int [20];
+         for(int i=0; i<=19;i++){
+            vetor[i] = i;
+        }
+           
+    }
+    
+}
+//for(int i=1; i<=num;i++){;;;;;
+//            soma = soma + i;
+//        }
+//        System.out.println("Foi somado:"+soma);
+//        
+//        ///////////////////////////////////////////
+//        int array[] = new int [10];
+//        
+//          for(int i=1; i<=9;i++){
+//            array[i] = i;
+//            System.out.println(array[i]);
+//        }
+//          
+//          for(int i=1; i<=9;i++){
+//            som1 = array[i] + som1;
+//        }
+//          int total = (som1/10);
+//          System.out.println("A média total foi:"+total);
+            
+            /////////////////////////////////////////////////////
+//            int vetor[] = new int [10];;
+//            int indice=0, valor =0;
+//            
+//            while(indice < vetor.length){
+//                if(valor % 2 != 0){
+//                    vetor[indice]=valor;
+//                    indice++;
+//                }
+//                valor++;
+//            }
+//            for(int item: vetor){
+//                System.out.println(item+" ");
